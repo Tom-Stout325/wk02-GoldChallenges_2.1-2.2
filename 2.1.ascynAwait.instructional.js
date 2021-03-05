9fa9f6463cd14532f5c2acf2dd59300ecfc269f4
Tom Stout <tomstout@Toms-iMac.local>
2021-03-04 21:17:25 -0500

    2.1 Completed

--- 2.1.ascynAwait.instructional.js
+++ 2.1.ascynAwait.instructional.js
@@ -1,8 +1,17 @@
 /* 
     1. Write a fat arrow async function that does the following:
-        - uses await to fetch data from this endpoint - 'https://swapi.co/api/people/<number here>'
+        - uses await to fetch data from this endpoint - 'https://swapi.dev/api/people/1/'
         - uses await to jsonify the response received from the fetch
         - console.log's the results of the fetch before console.logging a message saying 'this should print last'
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+async function challengeTwo(){ 
+    await fetch('https://swapi.dev/api/people/1/')
+        await then(res => res.json())
+        .then(json => console.log(json))
+    console.log('this should print last')
+}
+
+challengeTwo();
